6c7104473032830b1f2b1deee8ae8effaba08d57
Lavanya Chandrappa <106539@tfcorp.com>
2021-12-31 20:28:37 +0530

    commit changes

--- 2.todo/todo.js
+++ 2.todo/todo.js
@@ -6,29 +6,38 @@ function MyTodo() {
         setval(event.target.value)
     }
    
-    let submit = (e) => {
-        if (e!= "") {
-            text.push(e)
+    let submit = (add) => {
+        if (add!= "") {
+            text.push(add)
             settext([...text])
             setval("")
         }
     }
 
-
+    let remove=(del)=>{
+        if(del>=0){
+            text.splice(del,1)
+            settext([...text])
+        }
+    }
     return <div>
-    <h1>TODO LIST</h1>
- 
+    <h1 className="some" >TODO LIST</h1>
+
     <div class="input-group mb-3">
-     <input type="text" value={val} onChange={(event)=>{submitvalue(event)}} />
+     <input  type="text" value={val} onChange={(event)=>{submitvalue(event)}} placeholder="add ur todo"/>
+    <br/>
      <br/>
-     <br/>
-     <button onClick={()=>{submit(val)}}>Submit</button>
+     <button onClick={()=>{submit(val)}} className="some">Add Item</button>
+    <br/>
     <br/>
     
    </div>
             {
                 text.map((text,idx)=>{
-                    return <p key={idx}>{text}</p>
+                    return <tr className="table-active">
+                    <td key={idx}>{text}</td>
+                    <td><button  className="some" onClick={()=>{remove(idx)}}>X</button></td></tr>
+                      
                 })
             }
            
